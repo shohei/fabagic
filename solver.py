5ba5d1c222a9f50862005de76bc502bf4d89c088
Shohei Aoki <shoaok@gmail.com>
2014-05-06 05:42:26 +0800

    modified, but still messy.

--- solver.py
+++ solver.py
@@ -6,6 +6,7 @@ fin = open('in.path').readlines()
 close_points = []
 coefs = []
 i=0
+dy = 5 
 close_points.append([])
 for line in fin:
     print close_points[i]
@@ -22,7 +23,6 @@ print 'close_points',
 print close_points
 print 'total closure: ',len(close_points)
 
-dy = 1
 def calcScanPath(close_point):
     
     #point = [[1,1],[3,5],[5,2]]
@@ -64,7 +64,7 @@ def calcScanPath(close_point):
         for i in range(total_path-1):
             current_ymin = min(p[i][1],p[i+1][1])
             current_ymax = max(p[i][1],p[i+1][1])
-            print current_ymin,current_ymax
+            #print current_ymin,current_ymax
             if (y > current_ymin) and (y < current_ymax):
                 #hit path
                 hit.append(i)
@@ -72,13 +72,13 @@ def calcScanPath(close_point):
         print hit
         return hit
     
-    
     #hit_path(1.2,total_path)
     #hit_path(2.4,total_path)
     
     scanpath = []
     while(y < ymax):
         hits = hit_path(y,total_path) 
+        xhits = []
         for h in hits:
             a = coef['a'][h]
             b = coef['b'][h]
@@ -86,22 +86,46 @@ def calcScanPath(close_point):
                 xhit = a
             else:
                 xhit = round((y-b)*1.0/a,3)
-            y = round(y,3)
-            scanpath.append([xhit,y])
+                xhits.append(xhit)    
+        xhits.sort()
+        y = round(y,3)
+        for xh in xhits:
+            scanpath.append([xh,y])
 
         y = y + dy
-    
+    print scanpath 
     return scanpath
 
 
 total_scanpath = []
-debugDict = {'point':[],'coef':[],'xmin':[],'xmax':[],'ymin':[],'ymax':[],'scanpath':[]}
 for close_point in close_points:
     scanpath = calcScanPath(close_point)
     total_scanpath.append(scanpath)
 
+#print total_scanpath
 
-#TODO: this is not functioning well...
+fout = open('out.path','w')
+for scanpath in total_scanpath:
+    dic = {}
+    for s in scanpath:
+        sx = str(int(floor(s[0])))
+        sy = str(int(floor(s[1])))
+        try:
+            dic[sy].append(sx) 
+        except:
+            dic[sy]=[]
+            dic[sy].append(sx)
+    yall = dic.keys()
+    yall.sort()
+    for y in yall:
+        xall = dic[y]
+        xall.sort()
+        for x in xall:
+            fout.write(x+" "+y+" \n")
+    fout.write("-1 -1 \n")
+
+
+#TODO: this display function is not working well...
 def debugPlot(close_points,coefs,total_scanpath):
     #for loop for one closure
     i = 0
