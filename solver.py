c1fa4e9c4c6836dba9a7ab83549995ed44353159
Shohei Aoki <shoaok@gmail.com>
2014-05-05 15:12:01 +0800

    implemented, but not working matplotlib

--- solver.py
+++ solver.py
@@ -4,6 +4,7 @@ import math
 
 fin = open('in.path').readlines()
 close_points = []
+coefs = []
 i=0
 close_points.append([])
 for line in fin:
@@ -21,7 +22,7 @@ print 'close_points',
 print close_points
 print 'total closure: ',len(close_points)
 
-
+dy = 1
 def calcScanPath(close_point):
     
     #point = [[1,1],[3,5],[5,2]]
@@ -42,13 +43,17 @@ def calcScanPath(close_point):
     print p
     for i in range(len(p)-1):
         print p[i],p[i+1]
-        a = (p[i+1][1] - p[i][1])*1.0/(p[i+1][0] - p[i][0])
-        b = p[i][1] - a * p[i][0] 
+        try:
+            a = (p[i+1][1] - p[i][1])*1.0/(p[i+1][0] - p[i][0])
+            b = p[i][1] - a * p[i][0] 
+        except:
+            a = p[i+1][0] # Hey, this is foolish, but I'm inserting X value into A.
+            b = "NA"
         coef['a'].append(a)
         coef['b'].append(b)
     print coef
-    
-    dy=0.1
+    coefs.append(coef)    
+
     y = ymin
     total_path = int(len(point))
     print 'totalpath',
@@ -77,9 +82,13 @@ def calcScanPath(close_point):
         for h in hits:
             a = coef['a'][h]
             b = coef['b'][h]
-            xhit = round((y-b)*1.0/a,3)
+            if b == "NA":
+                xhit = a
+            else:
+                xhit = round((y-b)*1.0/a,3)
             y = round(y,3)
             scanpath.append([xhit,y])
+
         y = y + dy
     
     return scanpath
@@ -90,26 +99,62 @@ debugDict = {'point':[],'coef':[],'xmin':[],'xmax':[],'ymin':[],'ymax':[],'scanp
 for close_point in close_points:
     scanpath = calcScanPath(close_point)
     total_scanpath.append(scanpath)
-    debugDict
 
 
-def debugPlot(point,coef,xmin,xmax,ymin,ymax,scanpath):
-    for p in point:
-        plot(p[0],p[1],'o')
-    for i in range(len(coef['a'])):
-        x = arange(xmin,xmax,0.1)
-        y =  coef['a'][i] * x + coef['b'][i]
-        plot(x,y)
-    for s in scanpath:
-        x = s[0]
-        y = s[1]
-        plot(x,y,'o')
-    xlim([xmin-1,xmax+1])
-    ylim([ymin-1,ymax+1])
+#TODO: this is not functioning well...
+def debugPlot(close_points,coefs,total_scanpath):
+    #for loop for one closure
+    i = 0
+    g_x = []
+    g_y = []
+    for point in close_points:
+        xvec = []
+        yvec = []
+        for p in point:
+            plot(p[0],p[1],'o')
+            xvec.append(p[0])            
+            yvec.append(p[1])            
+        xmax = max(xvec)
+        xmin = min(xvec) 
+        ymax = max(yvec) 
+        ymin = min(yvec) 
+        g_x.append(xmax)
+        g_x.append(xmin)
+        g_y.append(ymax)
+        g_y.append(ymin)
+    
+        for j in range(len(coefs[i]['a'])):
+            if coefs[i]['b'][j]=="NA":
+                #y =  float(coefs[i]['a'][j]) * x + float(coefs[i]['b'][j])
+                yNum = int(floor((ymax-ymin)/dy))
+                yk = ymin
+                ylist = []
+                xlist = []
+                for k in range(yNum):
+                    ylist.append(y)
+                    xlist.append(xmin)
+                    yk = yk + dy
+                y = array(ylist)
+                x = array(xlist)
+            else:
+                x = arange(xmin,xmax,0.1)
+                y =  float(coefs[i]['a'][j]) * x + float(coefs[i]['b'][j])
+            plot(x,y)
+
+    for scanpath in total_scanpath:
+        for s in scanpath:
+            x = s[0]
+            y = s[1]
+            plot(x,y,'o')
+
+    g_xmin = min(g_x)
+    g_xmax = max(g_x)
+    g_ymin = min(g_y)
+    g_ymax = max(g_y)
+
+    xlim([g_xmin-1,g_xmax+1])
+    ylim([g_ymin-1,g_ymax+1])
     plt.show()
 
-for i in range():
-    #debugPlot(debugDict['point'][i],debugDict['coef'][i],debugDict['xmin'][i],debugDict['point'][i],debugDict['point'][i],debugDict['point'][i],debugDict['point'][i],debugDict['point'][i],)
-
-debugPlot
+#debugPlot(close_points,coefs,total_scanpath)
 
